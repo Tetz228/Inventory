1bee774b2a6789b1c9b49cda193d4beb98a58d15
Tetz <tetz2281337@gmail.com>
2021-02-23 21:20:29 +0700

    Добавление ViewModel

--- Inventory/ViewModels/MainViewModel.cs
+++ Inventory/ViewModels/MainViewModel.cs
@@ -1,10 +1,27 @@
 ﻿namespace Inventory.ViewModels
 {
     using DevExpress.Mvvm;
+    using Inventory.Pages;
+    using System.Windows.Controls;
+    using System.Windows.Input;
 
     public class MainViewModel : BindableBase
     {
+        public TablesEmployeesPage Emloyees { get; }
+        public ComputersPage Computers { get; }
 
+        public Page CurrentPage { get; set; }
+
+        public MainViewModel()
+        {
+            Emloyees = new TablesEmployeesPage();
+            Computers = new ComputersPage();
+
+            CurrentPage = Emloyees;
+        }
+
+        public ICommand OpenPageComputers => new DelegateCommand(() => CurrentPage = Computers);
+
+        public ICommand OpenPageEmloyees => new DelegateCommand(() => CurrentPage = Emloyees);
     }
-
 }
---
+++ Inventory/ViewModels/TablesEmployeesViewModel.cs
@@ -0,0 +1,43 @@
+﻿namespace Inventory.ViewModels
+{
+    using System;
+
+    using DevExpress.Mvvm;
+    using System.Collections.ObjectModel;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    using Inventory.Pages;
+
+    class TablesEmployeesViewModel : BindableBase
+    {
+        public Page CurrentPage { get; set; }
+
+        public DepartamentsPage DepartamentsPage { get; set; }
+        public EmployeesPage EmployeesPage { get; set; }
+        public PostsPage PostsPage { get; set; }
+
+        public ICommand SelectTableDepartament => new DelegateCommand(() =>
+        {
+            CurrentPage = DepartamentsPage;
+        });
+
+        public ICommand SelectTableEmployee => new DelegateCommand(() =>
+        {
+            CurrentPage = EmployeesPage;
+        });
+
+        public ICommand SelectTablePost => new DelegateCommand(() =>
+        {
+            CurrentPage = PostsPage;
+        });
+
+        public TablesEmployeesViewModel()
+        {
+            DepartamentsPage = new DepartamentsPage();
+            EmployeesPage = new EmployeesPage();
+            PostsPage = new PostsPage();
+        }
+    }
+}
