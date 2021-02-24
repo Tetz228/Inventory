92d7f475427ee7b03b68b15c62e5cc1c9e05cadd
Кирилл Зыков <tetz2281337@gmail.com>
2021-02-24 16:07:36 +0700

    Добавление View для добавления и изменения отделов

---
+++ Inventory/View/DepartmentEditWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Inventory.View
+{
+    using System.Windows;
+
+    using Inventory.ViewModels;
+
+    /// <summary>
+    /// Логика взаимодействия для DepartamentEditWindow.xaml
+    /// </summary>
+    public partial class DepartmentEditWindow : Window
+    {
+        public DepartmentEditWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
