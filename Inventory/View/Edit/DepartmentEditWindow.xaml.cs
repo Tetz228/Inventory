0d45df9d124c16951d56db446e66881d436fefff
Кирилл Зыков <tetz2281337@gmail.com>
2021-02-25 15:45:04 +0700

    Оптимизация кода

---
+++ Inventory/View/Edit/DepartmentEditWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Inventory.View.Edit
+{
+    public partial class DepartmentEditWindow
+    {
+        public DepartmentEditWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
