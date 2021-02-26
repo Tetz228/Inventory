3022a3015246ef24bcbd44da756734ba1ee515c5
Кирилл Зыков <tetz2281337@gmail.com>
2021-02-26 14:26:02 +0700

    Добавлено окно изменение информации

---
+++ Inventory/ViewModels/Edit/PostEditViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.ViewModels.Edit
+{
+    using System.Windows;
+    using System.Windows.Input;
+
+    using DevExpress.Mvvm;
+
+    using Inventory.Model;
+
+    public class PostEditViewModel:BindableBase
+    {
+        public Post Post { get; set; }
+
+        public ICommand Edit => new DelegateCommand<Window>(editWindow =>
+        {
+            using var db = new InventoryEntities();
+            var findPost = db.Posts.SingleOrDefault(post => post.Id_post == Post.Id_post);
+
+            if (findPost == null)
+                MessageBox.Show("Объект не найден в базе данных!", "Ошибка при изменении должности", MessageBoxButton.OK, MessageBoxImage.Error);
+            else
+            {
+                findPost.Name = Post.Name;
+                db.SaveChanges();
+            }
+            editWindow.Close();
+        }, _ => !string.IsNullOrWhiteSpace(Post.Name));
+
+        public ICommand Cancel => new DelegateCommand<Window>(editWindow => editWindow.Close());
+    }
+}
