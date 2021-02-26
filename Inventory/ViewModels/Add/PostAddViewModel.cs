7cd8f1bcb3a4cb34826c8ede97b0e082f0b7eb3d
Кирилл Зыков <tetz2281337@gmail.com>
2021-02-26 14:25:37 +0700

    Создание ViewModel для добавления информации

---
+++ Inventory/ViewModels/Add/PostAddViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Inventory.ViewModels.Add
+{
+    using System.Windows;
+    using System.Windows.Input;
+
+    using DevExpress.Mvvm;
+
+    using Inventory.Model;
+
+    public class PostAddViewModel : BindableBase
+    {
+        public string PostName { get; set; }
+
+        public ICommand Add => new DelegateCommand<Window>(addWindow =>
+        {
+            using var db = new InventoryEntities();
+
+            var post = new Post
+            {
+                Name = PostName
+            };
+
+            db.Posts.Add(post);
+            db.SaveChanges();
+
+            addWindow.Close();
+        }, _ => !string.IsNullOrWhiteSpace(PostName));
+
+        public ICommand Cancel => new DelegateCommand<Window>(addWindow => addWindow.Close());
+    }
+}
