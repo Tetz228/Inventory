b8fae1a94db5804b1c8ecc67c53347a994c74366
Tetz <tetz2281337@gmail.com>
2021-02-28 23:16:44 +0700

    Отчистка код от мусора

--- Inventory/Pages/Tables/Employees/EmployeesPage.xaml.cs
+++ Inventory/Pages/Tables/Employees/EmployeesPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿namespace Inventory.Pages.Tables.Employees
 {
     using System.Windows.Controls;
-
-    /// <summary>
-    /// Логика взаимодействия для EmployeesPage.xaml
-    /// </summary>
     public partial class EmployeesPage : Page
     {
         public EmployeesPage()
