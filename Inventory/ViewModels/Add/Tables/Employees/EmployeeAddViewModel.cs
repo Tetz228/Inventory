fe833748be208a381621254a826f4a30fdf6e2c2
Кирилл Зыков <tetz2281337@gmail.com>
2021-03-02 16:11:41 +0700

    Реализация окна добавления нового сотрудника

--- Inventory/ViewModels/Add/Tables/Employees/EmployeeAddViewModel.cs
+++ Inventory/ViewModels/Add/Tables/Employees/EmployeeAddViewModel.cs
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace Inventory.ViewModels.Add.Tables.Employees
 {
-    public class EmployeeAddViewModel
+    using System.Collections.ObjectModel;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Input;
+
+    using DevExpress.Mvvm;
+
+    using Inventory.Model;
+
+    public class EmployeeAddViewModel:BindableBase
     {
-        
+        public Employee Employee { get; set; }
+        public static ObservableCollection<Post> Posts { get; set; }
+        public static ObservableCollection<Post> AddPosts { get; set; }
+
+        public string KeyWord { get; set; }
+
+        public EmployeeAddViewModel()
+        {
+            var db = new InventoryEntities();
+            Posts = new ObservableCollection<Post>(db.Posts);
+            Employee = new Employee();
+        }
+
+        public ICommand AddCommand => new DelegateCommand(()=>
+        {
+            Employee.Posts_employees1.Add(new Posts_employees());
+        });
+
+        public string Value { get; set; }
     }
 }
