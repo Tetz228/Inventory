f8469a4a20c6562941603f70c08facbc84efada0
Кирилл Зыков <tetz2281337@gmail.com>
2021-03-03 16:11:49 +0700

    Добавлена простенька валидация. Добавлен аргумент в команды для закрытия окна

--- Inventory/ViewModels/Add/Tables/Employees/EmployeeAddViewModel.cs
+++ Inventory/ViewModels/Add/Tables/Employees/EmployeeAddViewModel.cs
@@ -24,8 +24,34 @@
         public ObservableCollection<Employees_in_departments> EmployeesInDepartments { get; set; } = new();
         #endregion
 
+        private bool Validation()
+        {
+            if (string.IsNullOrWhiteSpace(LastName))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(FirstName))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(MiddleName))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(PhoneNumber))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(Email))
+                return false;
+
+            if (PostsEmployees.Count == 0)
+                return false;
+
+            if (EmployeesInDepartments.Count == 0)
+                return false;
+
+            return true;
+        }
+
         #region Команды
-        public ICommand AddEmployee => new DelegateCommand( ()=>
+        public ICommand AddEmployee => new DelegateCommand<Window>(empAddWindow=>
         {
             using var db = new InventoryEntities();
 
@@ -52,7 +78,12 @@
 
             db.Employees_in_departments.AddRange(EmployeesInDepartments);
             db.SaveChanges();
-        });
+
+            empAddWindow.Close();
+
+        },_=> Validation());
+
+        public ICommand Cancel => new DelegateCommand<Window>(empAddWindow => empAddWindow.Close());
 
         public ICommand AddPostInCollection => new DelegateCommand(() => PostsEmployees.Add(new Posts_employees()));
 
