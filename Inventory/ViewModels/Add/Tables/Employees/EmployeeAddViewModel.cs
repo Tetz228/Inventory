d6b3f7847517198a26966800f15fac683694f7f0
Кирилл Зыков <tetz2281337@gmail.com>
2021-03-04 11:57:48 +0700

    Добавление регионов. Добавлена валидация почты

--- Inventory/ViewModels/Add/Tables/Employees/EmployeeAddViewModel.cs
+++ Inventory/ViewModels/Add/Tables/Employees/EmployeeAddViewModel.cs
@@ -1,13 +1,23 @@
 ﻿namespace Inventory.ViewModels.Add.Tables.Employees
 {
+    using System;
+
     using DevExpress.Mvvm;
     using Inventory.Model;
     using System.Collections.ObjectModel;
+    using System.Globalization;
+    using System.Text.RegularExpressions;
     using System.Windows;
     using System.Windows.Input;
 
     public class EmployeeAddViewModel : BindableBase
     {
+        public EmployeeAddViewModel()
+        {
+            PostsEmployees.Add(new Posts_employees());
+            EmployeesInDepartments.Add(new Employees_in_departments());
+        }
+
         #region Свойства
         public string LastName { get; set; }
 
@@ -24,7 +34,8 @@
         public ObservableCollection<Employees_in_departments> EmployeesInDepartments { get; set; } = new();
         #endregion
 
-        private bool Validation()
+        #region Методы валидации данных
+        private bool IsValidation()
         {
             if (string.IsNullOrWhiteSpace(LastName))
                 return false;
@@ -32,13 +43,10 @@
             if (string.IsNullOrWhiteSpace(FirstName))
                 return false;
 
-            if (string.IsNullOrWhiteSpace(MiddleName))
-                return false;
-
             if (string.IsNullOrWhiteSpace(PhoneNumber))
                 return false;
 
-            if (string.IsNullOrWhiteSpace(Email))
+            if (!IsValidEmail(Email))
                 return false;
 
             if (PostsEmployees.Count == 0)
@@ -50,8 +58,49 @@
             return true;
         }
 
+        private static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                    RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                string DomainMapper(Match match)
+                {
+                    var idn = new IdnMapping();
+
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+        #endregion
+
         #region Команды
-        public ICommand AddEmployee => new DelegateCommand<Window>(empAddWindow=>
+        public ICommand AddEmployee => new DelegateCommand<Window>(empAddWindow =>
         {
             using var db = new InventoryEntities();
 
@@ -81,7 +130,7 @@
 
             empAddWindow.Close();
 
-        },_=> Validation());
+        }, _ => IsValidation());
 
         public ICommand Cancel => new DelegateCommand<Window>(empAddWindow => empAddWindow.Close());
 
