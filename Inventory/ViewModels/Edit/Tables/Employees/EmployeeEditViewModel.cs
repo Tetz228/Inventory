8fe0a0872bab95a40bc0979adf1bfe7d50d69879
Кирилл Зыков <tetz2281337@gmail.com>
2021-03-04 15:30:32 +0700

    Добавлены свойства, методы валидации, команды

--- Inventory/ViewModels/Edit/Tables/Employees/EmployeeEditViewModel.cs
+++ Inventory/ViewModels/Edit/Tables/Employees/EmployeeEditViewModel.cs
@@ -1,13 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Inventory.ViewModels.Edit.Tables.Employees
+﻿namespace Inventory.ViewModels.Edit.Tables.Employees
 {
-    public class EmployeeEditViewModel
+    using DevExpress.Mvvm;
+    using Inventory.Model;
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Data.Entity;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public class EmployeeEditViewModel : BindableBase
     {
-        
+        #region Свойства
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public ObservableCollection<Posts_employees> PostsEmployees { get; set; }
+
+        public ObservableCollection<Employees_in_departments> EmployeesInDepartments { get; set; }
+        #endregion
+
+        #region Методы валидации данных
+        private bool IsValidation()
+        {
+            if (string.IsNullOrWhiteSpace(LastName))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(FirstName))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(PhoneNumber))
+                return false;
+
+            if (!IsValidEmail(Email))
+                return false;
+
+            if (PostsEmployees.Count == 0)
+                return false;
+
+            if (EmployeesInDepartments.Count == 0)
+                return false;
+
+            return true;
+        }
+
+        private static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                    RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                string DomainMapper(Match match)
+                {
+                    var idn = new IdnMapping();
+
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Команды
+        public ICommand EditEmployee => new DelegateCommand<Window>(empEditWindow =>
+        {
+            using var db = new InventoryEntities();
+
+            PostsEmployees.Add(new Posts_employees());
+           
+
+            //var employee = new Employee
+            //{
+            //    L_name = LastName,
+            //    F_name = FirstName,
+            //    M_name = MiddleName,
+            //    Phone_number = PhoneNumber,
+            //    Email = this.Email
+            //};
+
+            //db.Employees.Add(employee);
+            //db.SaveChanges();
+
+            //foreach (var post in PostsEmployees)
+            //    post.Fk_employee = employee.Id_employee;
+
+            //db.Posts_employees.AddRange(PostsEmployees);
+            //db.SaveChanges();
+
+            //foreach (var department in EmployeesInDepartments)
+            //    department.Fk_employee = employee.Id_employee;
+
+            //db.Employees_in_departments.AddRange(EmployeesInDepartments);
+            //db.SaveChanges();
+
+            empEditWindow.Close();
+
+        }, _ => IsValidation());
+
+        public ICommand Cancel => new DelegateCommand<Window>(empEditWindow =>
+        {
+            PostsEmployees.Add(new Posts_employees());
+            empEditWindow.Close(); 
+
+        });
+
+        public ICommand AddPostInCollection => new DelegateCommand(() => PostsEmployees.Add(new Posts_employees()));
+
+        public ICommand DeletePostFromCollection => new DelegateCommand<Posts_employees>((postEmp) => PostsEmployees.Remove(postEmp));
+
+        public ICommand AddDepartmentInCollection => new DelegateCommand(() => EmployeesInDepartments.Add(new Employees_in_departments()));
+
+        public ICommand DeleteDepartmentFromCollection => new DelegateCommand<Employees_in_departments>((empInDepart) => EmployeesInDepartments.Remove(empInDepart));
+        #endregion
     }
 }
