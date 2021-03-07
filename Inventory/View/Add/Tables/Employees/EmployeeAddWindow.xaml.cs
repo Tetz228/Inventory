1ebca34a4a61b52e0c11e690c7ef0cae6ab7b183
Tetz <tetz2281337@gmail.com>
2021-03-08 01:40:08 +0700

    Добавлен DataContext и событие Closing

--- Inventory/View/Add/Tables/Employees/EmployeeAddWindow.xaml.cs
+++ Inventory/View/Add/Tables/Employees/EmployeeAddWindow.xaml.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace Inventory.View.Add.Tables.Employees
+﻿namespace Inventory.View.Add.Tables.Employees
 {
-    /// <summary>
-    /// Логика взаимодействия для EmployeeAddWindow.xaml
-    /// </summary>
+    using Inventory.ViewModels.Add.Tables.Employees;
+    using System.Windows;
+
     public partial class EmployeeAddWindow : Window
     {
         public EmployeeAddWindow()
         {
             InitializeComponent();
+
+            var employeeAddViewModel = new EmployeeAddViewModel();
+            DataContext = employeeAddViewModel;
+            Closing += employeeAddViewModel.OnWindowClosing;
         }
     }
 }
