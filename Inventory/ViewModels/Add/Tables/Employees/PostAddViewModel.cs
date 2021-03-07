98ae34745d6a4ee2f152f8a7dee12d3586b34fc0
Tetz <tetz2281337@gmail.com>
2021-03-07 16:29:12 +0700

    Убрана валидация. Добавлено новое свойство

--- Inventory/ViewModels/Add/Tables/Employees/PostAddViewModel.cs
+++ Inventory/ViewModels/Add/Tables/Employees/PostAddViewModel.cs
@@ -2,48 +2,17 @@ namespace Inventory.ViewModels.Add.Tables.Employees
 {
     using DevExpress.Mvvm;
     using Inventory.Model;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Linq;
     using System.Windows;
     using System.Windows.Input;
 
-    public class PostAddViewModel : BindableBase, IDataErrorInfo
+    public class PostAddViewModel : BindableBase
     {
-        #region Свойства
-        public Dictionary<string, string> ErrorCollection { get; private set; } = new();
-
-        public string this[string name]
+        public PostAddViewModel()
         {
-            get
-            {
-                string result = null;
-
-                switch (name)
-                {
-                    case "PostName":
-                        if (string.IsNullOrWhiteSpace(PostName))
-                            result = "Поле не должно быть пустым";
-                        else if (PostName.Length < 2)
-                            result = "Поле должно содержать минимум 2 символа";
-                        break;
-                }
-
-                if (ErrorCollection.ContainsKey(name))
-                    ErrorCollection[name] = result;
-                else if (result != null)
-                    ErrorCollection.Add(name, result);
-
-                RaisePropertyChanged(nameof(ErrorCollection));
-
-                return result;
-            }
+            Post = new Post();
         }
 
-        public string Error { get => null; }
-
-        public string PostName { get; set; }
-        #endregion
+        public Post Post { get; }
 
         #region Команды
         public ICommand AddCommand => new DelegateCommand<Window>(addWindow =>
@@ -51,18 +20,14 @@ namespace Inventory.ViewModels.Add.Tables.Employees
             Add();
             
             addWindow.Close();
-        }, _ => Validation());
+        }, _ => Post.Validation());
 
         public ICommand Cancel => new DelegateCommand<Window>(addWindow => addWindow.Close());
         #endregion
 
-        #region Методы
-        private bool Validation() => ErrorCollection.Any(item => item.Value == null);
-
         private async void Add()
         {
-            await Post.AddPost(PostName);
+            await Post.AddPost(Post.Name);
         }
-        #endregion
     }
 }
