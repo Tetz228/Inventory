86843d04eb20d313caa1ad5e60e46bbb523e31ef
Tetz <tetz2281337@gmail.com>
2021-03-07 16:30:45 +0700

    Убрана валидация

--- Inventory/ViewModels/Edit/Tables/Employees/PostEditViewModel.cs
+++ Inventory/ViewModels/Edit/Tables/Employees/PostEditViewModel.cs
@@ -2,13 +2,10 @@
 {
     using DevExpress.Mvvm;
     using Inventory.Model;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Linq;
     using System.Windows;
     using System.Windows.Input;
 
-    public class PostEditViewModel : BindableBase, IDataErrorInfo
+    public class PostEditViewModel : BindableBase
     {
         public PostEditViewModel(Post post)
         {
@@ -17,59 +14,24 @@
         }
 
         #region Свойства
-        private Dictionary<string, string> ErrorCollection { get; set; } = new();
-
-        public string Error { get => null; }
-
-        public string this[string PostName]
-        {
-            get
-            {
-                string result = null;
-
-                switch (PostName)
-                {
-                    case "Post.Name":
-                        if (string.IsNullOrWhiteSpace(this.Post.Name))
-                            result = "Поле не должно быть пустым";
-                        else if (Post.Name.Length < 2)
-                            result = "Поле должно содержать минимум 2 символа";
-                        break;
-                }
-
-                if (ErrorCollection.ContainsKey(PostName))
-                    ErrorCollection[PostName] = result;
-                else if (result != null)
-                    ErrorCollection.Add(PostName, result);
-
-                RaisePropertyChanged(nameof(ErrorCollection));
-
-                return result;
-            }
-        }
-        public Post Post { get; set; }
+        public Post Post { get; }
         #endregion
 
         #region Команды
         public ICommand EditCommand => new DelegateCommand<Window>(editWindow =>
         {
             Post.EndEdit();
-
             Edit();
-
             editWindow.Close();
-        }, _ => Validation());
+        }, _ => Post.Validation());
 
         public ICommand Cancel => new DelegateCommand<Window>(editWindow =>
         {
             Post.CancelEdit();
-
             editWindow.Close();
         });
         #endregion
 
         private async void Edit() => await Post.EditPost(Post);
-
-        private bool Validation() => ErrorCollection.Any(item => item.Value == null);
     }
 }
