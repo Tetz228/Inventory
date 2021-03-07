5e84bc8cb0270f57ea459ebce9472bd7f169b1f3
Tetz <tetz2281337@gmail.com>
2021-03-07 17:14:53 +0700

    Добавлена статическая коллекция. Переписаны команды

--- Inventory/ViewModels/Tables/Employees/DepartmentViewModel.cs
+++ Inventory/ViewModels/Tables/Employees/DepartmentViewModel.cs
@@ -4,13 +4,9 @@
     using Inventory.Model;
     using Inventory.View.Add.Tables.Employees;
     using Inventory.View.Edit.Tables.Employees;
-    using Inventory.ViewModels.Add.Tables.Employees;
     using Inventory.ViewModels.Edit.Tables.Employees;
-    using System;
     using System.Collections.ObjectModel;
     using System.ComponentModel;
-    using System.Linq;
-    using System.Windows;
     using System.Windows.Data;
     using System.Windows.Input;
 
@@ -19,12 +15,12 @@
         public DepartmentViewModel()
         {
             using var db = new InventoryEntities();
-            Departments = new ObservableCollection<Department>(db.Departments.ToList());
+            Departments = new ObservableCollection<Department>(db.Departments);
             DepartmentsCollection = CollectionViewSource.GetDefaultView(Departments);
         }
 
         #region Свойства
-        public ObservableCollection<Department> Departments { get; private set; }
+        public static ObservableCollection<Department> Departments { get; set; }
 
         private ICollectionView DepartmentsCollection { get; set; }
 
@@ -55,59 +51,24 @@
 
         public ICommand AddDepartment => new DelegateCommand(() =>
         {
-            using var db = new InventoryEntities();
             var addDepartmentWindow = new DepartmentAddWindow();
-            addDepartmentWindow.ShowDialog();
 
-            Departments = new ObservableCollection<Department>(db.Departments.ToList());
-            DepartmentsCollection = CollectionViewSource.GetDefaultView(Departments);
+            addDepartmentWindow.ShowDialog();
         });
 
         public ICommand EditDepartment => new DelegateCommand<Department>((depart) =>
         {
-            using var db = new InventoryEntities();
             var editDepartmentWindow = new DepartmentEditWindow();
-            var editDepartmentViewModel = new DepartmentEditViewModel
-            {
-                Department = depart
-            };
+            var editDepartmentViewModel = new DepartmentEditViewModel(depart);
 
             editDepartmentWindow.DataContext = editDepartmentViewModel;
             editDepartmentWindow.ShowDialog();
 
-            Departments = new ObservableCollection<Department>(db.Departments.ToList());
-            DepartmentsCollection = CollectionViewSource.GetDefaultView(Departments);
         }, (depart) => depart != null);
 
-        public ICommand DeleteDepartment => new DelegateCommand<Department>((depart) =>
-        {
-            if (MessageBoxResult.Yes != MessageBox.Show($"Вы действительно хотите удалить {depart.Name}?",
-                "Удаление отдела", MessageBoxButton.YesNo, MessageBoxImage.Question))
-                return;
-
-            using var db = new InventoryEntities();
-            var findDepartment = db.Departments.SingleOrDefault(department => department.Id_department == depart.Id_department);
-
-            if (findDepartment == null)
-            {
-                MessageBox.Show("Объект не найден в базе данных!", "Ошибка при удалении отдела", MessageBoxButton.OK, MessageBoxImage.Error);
-                Departments = new ObservableCollection<Department>(db.Departments.ToList());
-                return;
-            }
-
-            try
-            {
-                db.Departments.Remove(findDepartment);
-                db.SaveChanges();
-
-                Departments.Remove(depart);
-            }
-            catch (System.Data.Entity.Infrastructure.DbUpdateException)
-            {
-                MessageBox.Show("Невозможно удалить отдел, так как он связан с другими сущностями!",
-                    "Ошибка при удалении отдела", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }, (depart) => depart != null);
+        public ICommand DeleteDepartment => new DelegateCommand<Department>(Delete, (selectDepart) => selectDepart != null);
         #endregion
+
+        private async void Delete(Department department) => await Department.DeleteDepartment(department);
     }
 }
