546719573d66f97f5b97afa0d4f9319ff4b5d377
Tetz <tetz2281337@gmail.com>
2021-03-08 19:09:39 +0700

    Переписан метод изменение сотрудника

--- Inventory/ViewModels/Edit/Tables/Employees/EmployeeEditViewModel.cs
+++ Inventory/ViewModels/Edit/Tables/Employees/EmployeeEditViewModel.cs
@@ -2,122 +2,49 @@
 {
     using DevExpress.Mvvm;
     using Inventory.Model;
-    using System;
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Data.Entity;
-    using System.Globalization;
-    using System.Linq;
-    using System.Text.RegularExpressions;
+    using System.ComponentModel;
     using System.Windows;
     using System.Windows.Input;
 
     public class EmployeeEditViewModel : BindableBase
     {
-        #region Свойства
+        public EmployeeEditViewModel(Employee employee)
+        {
+            Employee = employee;
+            Employee.BeginEdit();
+        }
+
+        public void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            Employee.PostsEmployees.Clear();
+            Employee.EmployeesInDepartments.Clear();
+        }
+
         public Employee Employee { get; set; }
-
-        public ObservableCollection<Posts_employees> PostsEmployees { get; set; }
-
-        public ObservableCollection<Employees_in_departments> EmployeesInDepartments { get; set; }
-
-        public List<Post> CollectionPosts { get; set; }
-
-        public List<Department> CollectionDepartments { get; set; }
-        #endregion
-
-        public EmployeeEditViewModel()
-        {
-            using var db = new InventoryEntities();
-            CollectionPosts = new List<Post>(db.Posts);
-            CollectionDepartments = new List<Department>(db.Departments);
-        }
-
-        #region Методы валидации данных
-        private bool IsValidation()
-        {
-            if (string.IsNullOrWhiteSpace(Employee.L_name))
-                return false;
-
-            if (string.IsNullOrWhiteSpace(Employee.F_name))
-                return false;
-
-            if (string.IsNullOrWhiteSpace(Employee.Phone_number))
-                return false;
-
-            if (!IsValidEmail(Employee.Email))
-                return false;
-
-            if (Employee.Posts_employees.Count == 0)
-                return false;
-
-            if (Employee.Employees_in_departments.Count == 0)
-                return false;
-
-            return true;
-        }
-
-        private static bool IsValidEmail(string email)
-        {
-            if (string.IsNullOrWhiteSpace(email))
-                return false;
-
-            try
-            {
-                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
-                    RegexOptions.None, TimeSpan.FromMilliseconds(200));
-
-                string DomainMapper(Match match)
-                {
-                    var idn = new IdnMapping();
-
-                    string domainName = idn.GetAscii(match.Groups[2].Value);
-
-                    return match.Groups[1].Value + domainName;
-                }
-            }
-            catch (RegexMatchTimeoutException)
-            {
-                return false;
-            }
-            catch (ArgumentException)
-            {
-                return false;
-            }
-
-            try
-            {
-                return Regex.IsMatch(email,
-                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
-                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-            }
-            catch (RegexMatchTimeoutException)
-            {
-                return false;
-            }
-        }
-        #endregion
 
         #region Команды
         public ICommand EditEmployee => new DelegateCommand<Window>(empEditWindow =>
         {
-            
+            Employee.EndEdit();
+            Edit();
             empEditWindow.Close();
+        }, _ => Employee.IsValidationCollections() && Employee.IsValidationProperties());
 
-        }, _ => IsValidation());
-
-        public ICommand Cancel => new DelegateCommand<Window>(empEditWindow =>
+        public ICommand Cancel => new DelegateCommand<Window>(empAddWindow =>
         {
-            
+            Employee.CancelEdit();
+            empAddWindow.Close();
         });
 
-        public ICommand AddPostInCollection => new DelegateCommand(() => PostsEmployees.Add(new Posts_employees()));
+        public ICommand AddPostInCollection => new DelegateCommand(() => Employee.PostsEmployees.Add(new Posts_employees()));
 
-        public ICommand DeletePostFromCollection => new DelegateCommand<Posts_employees>((postEmp) => PostsEmployees.Remove(postEmp));
+        public ICommand AddDepartmentInCollection => new DelegateCommand(() => Employee.EmployeesInDepartments.Add(new Employees_in_departments()));
 
-        public ICommand AddDepartmentInCollection => new DelegateCommand(() => EmployeesInDepartments.Add(new Employees_in_departments()));
+        public ICommand DeletePostFromCollection => new DelegateCommand<Posts_employees>(Employee.DeletePostFromCollection);
 
-        public ICommand DeleteDepartmentFromCollection => new DelegateCommand<Employees_in_departments>((empInDepart) => EmployeesInDepartments.Remove(empInDepart));
+        public ICommand DeleteDepartmentFromCollection => new DelegateCommand<Employees_in_departments>(Employee.DeleteDepartmentFromCollection);
         #endregion
+
+        private async void Edit() => await Employee.EditEmployee(Employee);
     }
 }
