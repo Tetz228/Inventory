186ab4bd264762d82e4da0d5ff24d9310d734605
Кирилл Зыков <tetz2281337@gmail.com>
2021-03-11 11:52:28 +0700

    Добавлена новая таблица "Пользователи". Добавлено окно добавления нового пользователя

---
+++ Inventory/ViewModels/Add/Tables/Employees/UserAddViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.ViewModels.Add.Tables.Employees
+{
+    using System.Windows;
+    using System.Windows.Input;
+
+    using DevExpress.Mvvm;
+
+    using Inventory.Model;
+
+    public class UserAddViewModel : BindableBase
+    {
+        public UserAddViewModel()
+        {
+            User = new User();
+        }
+
+        public User User { get; }
+
+        #region Команды
+        public ICommand AddCommand => new DelegateCommand<Window>(addWindow =>
+        {
+            User.AddUser(User);
+            addWindow.Close();
+        }, _ => User.IsValidationProperties());
+
+        public ICommand Cancel => new DelegateCommand<Window>(addWindow => addWindow.Close());
+        #endregion
+    }
+}
