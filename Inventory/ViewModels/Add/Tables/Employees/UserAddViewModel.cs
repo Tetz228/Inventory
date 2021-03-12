df1b02cb15f63ad57071718ab8a44e53d315cc70
Кирилл Зыков <tetz2281337@gmail.com>
2021-03-12 14:12:37 +0700

    Добавлена коллекция с сотрудниками

--- Inventory/ViewModels/Add/Tables/Employees/UserAddViewModel.cs
+++ Inventory/ViewModels/Add/Tables/Employees/UserAddViewModel.cs
@@ -1,54 +1,47 @@
 ﻿namespace Inventory.ViewModels.Add.Tables.Employees
 {
-    using System;
-    using System.Runtime.InteropServices;
-
     using DevExpress.Mvvm;
     using Inventory.Model;
-    using System.Security;
-    using System.Security.Cryptography;
+    using System.Collections.ObjectModel;
     using System.Windows;
+    using System.Windows.Controls;
     using System.Windows.Input;
-
-    using BCrypt.Net;
 
     public class UserAddViewModel : BindableBase
     {
         public UserAddViewModel()
         {
             User = new User();
+            using var db = new InventoryEntities();
+            Employees = new ObservableCollection<Employee>(db.Employees);
         }
 
         public User User { get; }
 
-        public SecureString SecurePassword { private get; set; }
+        public ObservableCollection<Employee> Employees { get; set; }
 
         #region Команды
         public ICommand AddCommand => new DelegateCommand<Window>(addWindow =>
         {
-            BAD_ConvertToUnsecureString(SecurePassword);
-            //User.AddUser(User);
-
-
+            User.AddUser(User);
 
             addWindow.Close();
 
+        }, _ => User.IsValidationProperties() && User.ValidPassword() && User.Fk_employee != 0 && User.EqualsPasswords());
+
+        public ICommand PasswordChanged => new DelegateCommand<PasswordBox>(passwordBox =>
+        {
+            if (passwordBox != null)
+                User.Password = passwordBox.Password;
+
         }, _ => true);
 
-        public static string BAD_ConvertToUnsecureString(SecureString securePassword)
+        public ICommand PasswordChangedRepeated => new DelegateCommand<PasswordBox>(passwordBox =>
         {
-            IntPtr unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+            if (passwordBox != null)
+                User.PasswordRepeated = passwordBox.Password;
 
-            var password = Marshal.PtrToStringUni(unmanagedString);
-
-            Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
-
-            string salt = BCrypt.GenerateSalt(7);
-            var h =BCrypt.HashPassword(password, salt);
-            
-
-            return password;
-        }
+        }, _ => true);
 
         public ICommand Cancel => new DelegateCommand<Window>(addWindow => addWindow.Close());
         #endregion
