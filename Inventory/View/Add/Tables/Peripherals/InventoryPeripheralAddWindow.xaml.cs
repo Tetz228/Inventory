ccf63927526b6ad10fbc1ac56ae0765c05a37e14
Tetz <tetz2281337@gmail.com>
2021-03-19 07:27:59 +0700

    Добавлено окно добавления инвентарного номера

---
+++ Inventory/View/Add/Tables/Peripherals/InventoryPeripheralAddWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Inventory.View.Add.Tables.Peripherals
+{
+    /// <summary>
+    /// Логика взаимодействия для InventoryPeripheralAddWindow.xaml
+    /// </summary>
+    public partial class InventoryPeripheralAddWindow : Window
+    {
+        public InventoryPeripheralAddWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Inventory/ViewModels/Add/Tables/Peripherals/InventoryPeripheralAddViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Inventory.ViewModels.Add.Tables.Peripherals
+{
+    using DevExpress.Mvvm;
+    using Inventory.Model;
+    using System.Collections.ObjectModel;
+    using System.Windows;
+    using System.Windows.Input;
+
+    class InventoryPeripheralAddViewModel : BindableBase
+    {
+        public InventoryPeripheralAddViewModel()
+        {
+            using var db = new InventoryEntities();
+
+            InventoryNumberPeripheral = new Inventory_numbers_peripherals();
+            Peripherals = new ObservableCollection<Peripheral>(db.Peripherals);
+            StatusesPeripherals = new ObservableCollection<Statuses_peripherals>(db.Statuses_peripherals);
+        }
+
+        public Inventory_numbers_peripherals InventoryNumberPeripheral { get; }
+
+        public ObservableCollection<Peripheral> Peripherals { get; }
+
+        public ObservableCollection<Statuses_peripherals> StatusesPeripherals { get; }
+
+        #region Команды
+        public ICommand AddCommand => new DelegateCommand<Window>(addWindow =>
+        {
+            Inventory_numbers_peripherals.AddInventoryNumber(InventoryNumberPeripheral);
+            addWindow.Close();
+        }, _ => InventoryNumberPeripheral.IsValidationProperties());
+
+        public ICommand Cancel => new DelegateCommand<Window>(addWindow => addWindow.Close());
+        #endregion
+    }
+}
