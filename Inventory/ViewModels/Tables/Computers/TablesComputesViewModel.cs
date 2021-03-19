0ff0826d5124c0ed55b8cee277e22557e10bbd16
Кирилл Зыков <tetz2281337@gmail.com>
2021-03-19 13:27:34 +0700

    Добавлена ViewModel для таблиц связанных с компьютерами

---
+++ Inventory/ViewModels/Tables/Computers/TablesComputesViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.ViewModels.Tables.Computers
+{
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    using DevExpress.Mvvm;
+
+    using Inventory.Pages.Tables;
+
+    public class TablesComputesViewModel:BindableBase
+    {
+        public TablesComputesViewModel()
+        {
+            CurrentPage = ManufacturersPage = new ManufacturersPage();
+        }
+
+
+        #region Свойства
+        public Page CurrentPage { get; private set; } 
+
+        private ManufacturersPage ManufacturersPage { get; }
+        #endregion
+
+        #region Команды
+
+
+        public ICommand SelectTablesManufacturers => new DelegateCommand(() => CurrentPage = ManufacturersPage);
+        #endregion
+    }
+}
