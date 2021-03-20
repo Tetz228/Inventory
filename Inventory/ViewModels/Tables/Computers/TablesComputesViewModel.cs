cdc88d5aafc495da2d5f42f00cd548233ff1100e
Tetz <tetz2281337@gmail.com>
2021-03-20 19:20:48 +0700

    Добавлен вызов жд, типов жд, единиц измерения в ViewModel

--- Inventory/ViewModels/Tables/Computers/TablesComputesViewModel.cs
+++ Inventory/ViewModels/Tables/Computers/TablesComputesViewModel.cs
@@ -12,6 +12,9 @@
         {
             CurrentPage = SoketsPage = new SoketsPage();
             ManufacturersPage = new ManufacturersPage();
+            HddsPage = new HddsPage();
+            UnitsPage = new UnitsPage();
+            TypesHddPage = new TypesHddPage();
         }
 
         #region Свойства
@@ -19,11 +22,24 @@
 
         private ManufacturersPage ManufacturersPage { get; }
 
+        private HddsPage HddsPage { get; }
+
+        private UnitsPage UnitsPage { get; }
+
         private SoketsPage SoketsPage { get; }
+
+        private TypesHddPage TypesHddPage { get; }
+        
         #endregion
 
         #region Команды
         public ICommand SelectSoketsPage => new DelegateCommand(() => CurrentPage = SoketsPage);
+
+        public ICommand SelectHddsPage => new DelegateCommand(() => CurrentPage = HddsPage);
+
+        public ICommand SelectUnitsPage => new DelegateCommand(() => CurrentPage = UnitsPage);
+
+        public ICommand SelectTypesHddPage => new DelegateCommand(() => CurrentPage = TypesHddPage);
 
         public ICommand SelectTablesManufacturers => new DelegateCommand(() => CurrentPage = ManufacturersPage);
         #endregion
