1197bcacc990f2abf46709e3efae68c571cbf49e
Кирилл Зыков <tetz2281337@gmail.com>
2021-03-22 09:56:46 +0700

    Дописано изменение жесткого диска

--- Inventory/View/Edit/Tables/Computers/HddEditWindow.xaml.cs
+++ Inventory/View/Edit/Tables/Computers/HddEditWindow.xaml.cs
@@ -1,27 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace Inventory.View.Edit.Tables.Computers
+﻿namespace Inventory.View.Edit.Tables.Computers
 {
-    /// <summary>
-    /// Логика взаимодействия для HddEditWindow.xaml
-    /// </summary>
+    using System.Windows;
+
     public partial class HddEditWindow : Window
     {
         public HddEditWindow()
         {
             InitializeComponent();
+            TextBoxName.Focus();
         }
     }
 }
--- Inventory/ViewModels/Edit/Tables/Computers/HddEditViewModel.cs
+++ Inventory/ViewModels/Edit/Tables/Computers/HddEditViewModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace Inventory.ViewModels.Edit.Tables.Computers
 {
+    using System.Collections.ObjectModel;
+
     using DevExpress.Mvvm;
     using Inventory.Model;
     using System.ComponentModel;
@@ -10,11 +12,23 @@
     {
         public HddEditViewModel(Hdd hdd)
         {
+            using var db = new InventoryEntities();
+
+            Manufacturers = new ObservableCollection<Manufacturer>(db.Manufacturers);
+            TypesHdds = new ObservableCollection<Types_hdd>(db.Types_hdd);
+            Units = new ObservableCollection<Unit>(db.Units);
+
             Hdd = hdd;
             Hdd.BeginEdit();
         }
 
         public Hdd Hdd { get; }
+
+        public ObservableCollection<Manufacturer> Manufacturers { get; }
+
+        public ObservableCollection<Types_hdd> TypesHdds { get; }
+
+        public ObservableCollection<Unit> Units { get; }
 
         public void OnWindowClosing(object sender, CancelEventArgs e) => Hdd.CancelEdit();
 
