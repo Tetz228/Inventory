c66200a2dcb3a4f3cc38df8736222eb85149f965
Tetz <tetz2281337@gmail.com>
2021-03-30 23:08:32 +0700

    Добавлены функции смены пароля, смены пользователя

--- Inventory/ViewModels/MainViewModel.cs
+++ Inventory/ViewModels/MainViewModel.cs
@@ -1,15 +1,16 @@
 ﻿namespace Inventory.ViewModels
 {
     using DevExpress.Mvvm;
-
+    using Inventory.View.Pages.Tables.Computers;
+    using Inventory.View.Pages.Tables.Employees;
+    using Inventory.View.Pages.Tables.Peripherals;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
 
-    using Inventory.View.Pages.Tables.Computers;
-    using Inventory.View.Pages.Tables.Computers.Computers;
-    using Inventory.View.Pages.Tables.Employees;
-    using Inventory.View.Pages.Tables.Peripherals;
+    using Inventory.Model;
+    using Inventory.Services;
+    using Inventory.View;
 
     public class MainViewModel : BindableBase
     {
@@ -35,7 +36,20 @@
 
         public ICommand OpenPagePeripherals => new DelegateCommand(() => CurrentPage = Peripherals);
 
-        public ICommand Exit => new DelegateCommand(() => Application.Current.Shutdown());
+        public ICommand ChangePasswordCommand => new DelegateCommand(() =>
+        {
+            if (User.AuthorizedUser != null)
+                UsersInteraction.OnWindowChangePassword(User.AuthorizedUser.Id_user);
+        });
+
+        public ICommand ChangeUserCommand => new DelegateCommand<Window>(mainWindow =>
+        {
+            var authWindow = new AuthorizationWindow();
+            mainWindow.Close();
+            authWindow.Show();
+        });
+
+        public ICommand ExitCommand => new DelegateCommand(() => Application.Current.Shutdown());
 
         public ICommand OpenPageEmployees => new DelegateCommand(() => CurrentPage = Employees);
         #endregion
