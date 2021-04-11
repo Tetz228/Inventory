2f845b9e304446d530475f0323f7e635e780f3f3
Tetz <tetz2281337@gmail.com>
2021-04-11 16:19:05 +0700

    Добавлен метод конвертации из строки в число с плавающей точкой

---
+++ Inventory/Services/StringToDoubleConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Inventory.Services
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class StringToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => ConvertStringToDouble(value);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => ConvertStringToDouble(value);
+
+        private static object ConvertStringToDouble(object value)
+        {
+            if (value == null)
+                return null;
+
+            string doubleString = value.ToString();
+
+            return double.TryParse(doubleString, out double number) ? number : 0;
+        }
+    }
+}
