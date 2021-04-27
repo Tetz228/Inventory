86c053870bbe5a58bbc5b0c8d49531a3fb031438
Tetz <tetz2281337@gmail.com>
2021-04-27 11:29:06 +0700

    Попытка сделать изменение через коллекции в классе

--- Inventory/Model/Posts_employees.cs
+++ Inventory/Model/Posts_employees.cs
@@ -1,10 +1,32 @@
 namespace Inventory.Model
 {
-    public partial class Posts_employees
+    using System.Linq;
+
+    using DevExpress.Mvvm;
+
+    public partial class Posts_employees:BindableBase
     {
+        private int _fkPost;
+
         public int Id_post_employee { get; set; }
         public int Fk_employee { get; set; }
-        public int Fk_post { get; set; }
+
+        public int Fk_post
+        {
+            get => _fkPost;
+            set
+            {
+                _fkPost = value;
+                RaisePropertyChanged(nameof(Fk_post));
+
+                if (Fk_employee != 0)
+                {
+                    using var db = new InventoryEntities();
+
+                    Post = db.Posts.FirstOrDefault(post => post.Id_post == Fk_post);
+                }
+            }
+        }
 
         public virtual Employee Employee { get; set; }
         public virtual Post Post { get; set; }
