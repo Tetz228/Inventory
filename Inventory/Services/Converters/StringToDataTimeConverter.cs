0b8cabd828f022715ad32eac4feb0efb4a145fb8
Tetz <tetz2281337@gmail.com>
2021-04-30 14:31:21 +0700

    Добавлен конвертер даты

---
+++ Inventory/Services/Converters/StringToDataTimeConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace Inventory.Services.Converters
+{
+    using System;
+    using System.Windows;
+
+    public class StringToDataTimeConverter : BaseConverter
+    {
+        protected override object ConvertStringTo(object value) => ((DateTime)value).ToString("dd.MM.yyyy");
+
+        protected override object ConvertBackStringTo(object value) => DependencyProperty.UnsetValue;
+    }
+}
