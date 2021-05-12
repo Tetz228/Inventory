5033b8ac300cfae16f53197e9d29732f1fcc841e
Tetz <tetz2281337@gmail.com>
2021-05-12 21:50:58 +0700

    Мелкие правки

--- Inventory/View/Pages/Tables/Computers/Other/UnitsPage.xaml.cs
+++ Inventory/View/Pages/Tables/Computers/Other/UnitsPage.xaml.cs
@@ -2,14 +2,8 @@
 {
     using System.Windows.Controls;
 
-    /// <summary>
-    /// Логика взаимодействия для UnitsPage.xaml
-    /// </summary>
     public partial class UnitsPage : Page
     {
-        public UnitsPage()
-        {
-            InitializeComponent();
-        }
+        public UnitsPage() => InitializeComponent();
     }
 }
